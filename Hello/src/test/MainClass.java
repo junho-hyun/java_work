6f78576bbb8b68be3e9dcec452e5ba9fa74f03be
junho <tmddn8799@naver.com>
2022-04-28 17:01:50 +0900

    Hello 프로젝트 추가

---
+++ Hello/src/test/MainClass.java
@@ -0,0 +1,16 @@
+package test;
+
+public class MainClass {
+	//run(실행) 했을 때 실행의 흐름이 시작 되는 main 메소드
+	public static void main(String[] args) {
+		
+		System.out.println("Hello,World!");
+		//System.out.println 이것은 자바스크립트에 콘솔.log 같은거다.
+		
+		String str="kimgura";
+		
+		int num1=10;
+		
+		boolean isRun=true;
+	}
+}
